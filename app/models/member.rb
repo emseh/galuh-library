c09747c2bf14a3f7525b8b625e0a4aeebc062ee2
emseh <andrianrizkieffendi@gmail.com>
2021-06-18 19:36:16 +0700

    Set Validation Book and Member

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,8 @@
 class Member < ApplicationRecord
+    validates :full_name, presence: true
+    validates :address, presence: true
+    validates :phone, presence: true, length: { minimum: 10 }, numericality: { only_integer: true }
+    validates :email, presence: true 
+    validates :born_date, presence: true 
+
 end
